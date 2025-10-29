3ddd91797bc2b450d6d0a978a64400d39c11a62c
scratchLearner01 <shaurya8092@gmail.com>
2025-10-29 14:43:21 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // all your React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
